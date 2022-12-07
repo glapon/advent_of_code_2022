45d85058d63cb87b3158f7f26f4a87f19fbb8f17
Gary Lapon <glapon@gmail.com>
2022-12-06 20:53:52 -0800

    solve day 2 part 2

--- day_2/day2_part2.py
+++ day_2/day2_part2.py
@@ -3,18 +3,45 @@ class Rock:
         self.name = 'rock'
         self.score = 1
         self.beats = 'scissors'
+    #what to choose to achieve result against rock
+    def win(self):
+        return Paper()
+    
+    def lose(self):
+        return Scissors()
+
+    def draw(self):
+        return Rock()
 
 class Paper:
     def __init__(self):
         self.name = 'paper'
         self.score = 2
         self.beats = 'rock'
+    #what to choose to achieve result against rock
+    def win(self):
+        return Scissors()
+    
+    def lose(self):
+        return Rock()
+
+    def draw(self):
+        return Paper()
 
 class Scissors:
     def __init__(self):
         self.name = 'scissors'
         self.score = 3
         self.beats = 'paper'
+    #what to choose to achieve result against rock
+    def win(self):
+        return Rock()
+    
+    def lose(self):
+        return Paper()
+
+    def draw(self):
+        return Scissors()
 
 class Game:
     def __init__(self, opponent, player):
@@ -32,17 +59,20 @@ class Game:
 def make_move(letter):
     if letter == 'A': return Rock()
     elif letter == 'B': return Paper()
-    elif letter == 'C': return Scissors()     
+    elif letter == 'C': return Scissors()
 
-def get_outcome(letter):
+def your_move(letter, their_move):
+    if letter == 'X': return their_move.lose()
+    elif letter == 'Y': return their_move.draw()
+    elif letter == 'Z': return their_move.win()
        
-
 games = []
 # open file and make each game into objects
 with open('input.txt') as input:
     for game in input.readlines():
         game_split = game.strip('\n').split(' ')
-        games.append(Game(make_move(game_split[0]), make_move(game_split[1])))
+        their_move = make_move(game_split[0])
+        games.append(Game(their_move, your_move(game_split[1], their_move)))
 
 total_score = 0
 for game in games:
