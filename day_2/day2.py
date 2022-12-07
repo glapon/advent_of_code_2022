43cd6ce73dee853884b6c30921ae4821be436b54
Gary Lapon <glapon@gmail.com>
2022-12-06 20:36:22 -0800

    solve day 2 part 1

--- day_2/day2.py
+++ day_2/day2.py
@@ -16,10 +16,33 @@ class Scissors:
         self.score = 3
         self.beats = 'paper'
 
+class Game:
+    def __init__(self, opponent, player):
+        self.total_score = player.score
+        #beat them you get 6
+        if player.beats == opponent.name:
+            self.total_score += 6
+        # they don't beat you, draw, you get 3
+        elif player.name != opponent.beats:
+            self.total_score += 3
+    
+    def score_game(self):
+        return self.total_score
+
+def make_move(letter):
+    if letter in ['A', 'X']: return Rock()
+    elif letter in ['B', 'Y']: return Paper()
+    elif letter in ['C', 'Z']: return Scissors()        
+
 games = []
-# open file and add each line to calories
+# open file and make each game into objects
 with open('input.txt') as input:
     for game in input.readlines():
-        games.append(game.strip('\n').split(' '))
+        game_split = game.strip('\n').split(' ')
+        games.append(Game(make_move(game_split[0]), make_move(game_split[1])))
 
-print(games)
+total_score = 0
+for game in games:
+    total_score += game.score_game()
+
+print(total_score)
