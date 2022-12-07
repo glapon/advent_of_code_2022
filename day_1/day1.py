d1cc7ec7968319e4690237b7288a9cd7a16330c0
Gary Lapon <glapon@gmail.com>
2022-12-06 17:35:26 -0800

    solve day 1 part 2

--- day_1/day1.py
+++ day_1/day1.py
@@ -26,10 +26,26 @@ with open('input.txt') as input:
         elves.append(Elf(temp_list))
             
 most_calories = 0
+top_3 = []
+
 for elf in elves:
     cals = elf.get_total_cals()
     if cals > most_calories:
         most_calories = cals
+    if len(top_3) < 3:
+        top_3.append(cals)
+        top_3.sort()
+    else:
+        # replace the smallest one its bigger than, then sort again
+        for i in range(3):
+            if cals > top_3[i]:
+                top_3[i] = cals
+                top_3.sort()
+                break
 
-# calories of elf with most
+# calories of elf with most (part 1 answer)
 print(most_calories)
+sum_top_3 = 0
+for num in top_3:
+    sum_top_3 += num
+print(sum_top_3)
